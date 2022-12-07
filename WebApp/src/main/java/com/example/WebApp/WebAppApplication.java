980c492eb30b9fbc92a296abd06bff6be62f6cdc
Manpreet Panwar <Manpreet.Panwar@Geminisolutions.com>
2022-12-07 12:08:28 +0530

    webapp using springboot

---
+++ WebApp/src/main/java/com/example/WebApp/WebAppApplication.java
@@ -0,0 +1,21 @@
+package com.example.WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.xml.bind.SchemaOutputResolver;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(WebAppApplication.class, args);
+		System.out.println("han bhai kyu nhi hora ye..");
+		System.out.println("jb chrome pr chlega tb aayega mja to..");
+
+	}
+
+}
+
+
