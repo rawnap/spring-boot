554334b8deda8df1c5cfd50d4622436e788d65da
Manpreet Panwar <Manpreet.Panwar@Geminisolutions.com>
2022-12-19 21:29:33 +0530

    Spring Boot | Data JPA | MVC | H2 | Query Methods Example Part 3

---
+++ jpa_boot_2/src/main/java/com/example/jpa_boot_2/AlienController.java
@@ -0,0 +1,41 @@
+package com.example.jpa_boot_2;
+
+import com.example.jpa_boot_2.dao.AlienRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AlienController
+{
+
+    @Autowired
+    AlienRepo repo;
+    @RequestMapping("/")
+    public String home()
+    {
+        return"home.jsp";
+    }
+    @RequestMapping("/addAlien")
+    public String addAlien(Alien alien)
+    {
+        repo.save(alien);
+        return "home.jsp";
+    }
+
+    @RequestMapping("/getAlien")
+    public ModelAndView getAlien(@RequestParam int aid)
+    {
+        ModelAndView mv= new ModelAndView("ShowAlien.jsp");
+        Alien alien= repo.findById(aid).orElse(new Alien());
+
+        System.out.println(repo.findByTech("Java"));
+        System.out.println(repo.findByAidGreaterThan(102));
+        System.out.println(repo.findByTechSorted("Java"));
+
+        mv.addObject(alien);
+        return mv;
+    }
+}
---
+++ jpa_boot_2/src/test/java/com/example/jpa_boot_2/JpaBoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_boot_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBoot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
